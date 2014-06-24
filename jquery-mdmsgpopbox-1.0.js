1cedf3b3176cbc245ef8c35b864d8885f8cab824
mrtduzgun <mrtduzgun@gmail.com>
2014-06-24 11:22:28 +0300

    some bugs fixed

--- jquery-mdmsgpopbox-1.0.js
+++ jquery-mdmsgpopbox-1.0.js
@@ -6,92 +6,97 @@
  *  @date           27.12.12
  *  @author         Murat Duzgun
  *  @see            https://github.com/mrtduzgun/jquery-mdmsgpopbox
-*/
+ */
 
 (function($){
-    
+
     $.mdmsgpopbox = function(options) {
-        
+
         if (/1\.(0|1|2|3)\.(0|1|2)/.test($.fn.jquery)) {
             alert("It needs JQuery 1.3.2 version at least!");
             return false;
         }
 
         var msgBox = $(".mdMsgpopbox");
-        
+        var defaults;
+
         if (options && typeof options == "string") {
-            
+
+            var defaults = msgBox.data('opts');
+
             if (options == "hide")
                 hideMsgBox();
-            
+
             return true;
         }
-        
-        var defaults = $.extend($.mdmsgpopbox.defaults, options);
-        
+
+        defaults = $.extend($.mdmsgpopbox.defaults, options);
+        msgBox.data('opts', defaults);
+
         if (msgBox && msgBox.length > 0) {
             msgBox = $(".mdMsgpopbox");
             clearTimer();
 
             msgBox.children().removeClass().
-                addClass(defaults.cssClasses[defaults.msgType]).html(defaults.message);           
-            
+                addClass(defaults.cssClasses[defaults.msgType]).html(defaults.message);
+
             if (defaults.msgType && defaults.msgType != "loading") {
                 setTimer(hideMsgBox);
                 $(".mdMsgpopboxOverlay").hide();
             }
-            
+
             if (defaults.msgType && defaults.msgType == "loading")
                 $(".mdMsgpopboxOverlay").show();
-            
+
             msgBox.fadeIn("fast");
 
         } else {
 
             $("body").append('<div class="mdMsgpopbox">'+
-                            '<div class="'+ defaults.cssClasses[defaults.msgType] +'">'+
-                            defaults.message +'</div>'+
-                        '</div>');
+                '<div class="'+ defaults.cssClasses[defaults.msgType] +'">'+
+                defaults.message +'</div>'+
+                '</div>');
 
             msgBox = $(".mdMsgpopbox").fadeIn("fast");
-            
+
             if (defaults.msgType && defaults.msgType != "loading")
                 setTimer(hideMsgBox);
-            
+
             if (defaults.msgType && defaults.msgType == "loading")
                 $("<div class='mdMsgpopboxOverlay'/>").appendTo("body");
         }
-        
-        if (defaults.mouseKeyboardEvent && defaults.msgType && 
-                defaults.msgType != "loading")
+
+        if (defaults.mouseKeyboardEvent && defaults.msgType &&
+            defaults.msgType != "loading")
             bindEvents();
-        
+
         function hideMsgBox() {
-            msgBox.fadeOut("slow", function() {
-                if (defaults.finishCallback)
+            $(".mdMsgpopboxOverlay").hide();
+            msgBox.fadeOut("fast", function() {
+                if (defaults && defaults.finishCallback)
                     defaults.finishCallback.call();
             });
         }
-        
+
         function clearTimer() {
             if (msgBox.data("timer"))
                 clearTimeout(msgBox.data("timer"));
         }
-        
+
         function setTimer(callback) {
             msgBox.data("timer", setTimeout(callback, defaults.timeout));
         }
-        
+
         function bindEvents() {
             msgBox.bind("mousemove click keypress",function(e){
                 clearTimer();
                 hideMsgBox();
             });
         }
-        
+
         return true;
     }
-    
+
     $.mdmsgpopbox.defaults = {
         cssClasses: {
             success:    'mdMsgpopboxSuccess',
@@ -106,5 +111,5 @@
         msgType: 'success', // success, fail, warning, info, loading
         mouseKeyboardEvent: false
     };
-    
+
 })(jQuery);
